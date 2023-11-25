db5c403b4e91f185397592b5c1d52c2da4b91ae9
sichengchen <csc0330@hotmail.com>
2023-11-25 16:25:59 -0500

    Change background color of Code component to gray-200

--- components/typography/Code.tsx
+++ components/typography/Code.tsx
@@ -1,5 +1,5 @@
 const Code = (props: any) => {
-    return <code {...props} className={`text-sm p-[2px] rounded bg-gray-300 dark:bg-gray-700 ml-[0.25em] mr-[0.25em]`}>
+    return <code {...props} className={`text-sm p-[2px] rounded bg-gray-200 dark:bg-gray-700 ml-[0.25em] mr-[0.25em]`}>
         {props.children}
     </code>
 }
