29e940dea72a1d4adbe6ad98296e46613cea7d6e
sichengchen <csc0330@hotmail.com>
2024-02-17 17:13:45 -0500

    Fix edit link

--- components/Reader.tsx
+++ components/Reader.tsx
@@ -7,7 +7,7 @@ const Reader = ({title, subtitle, content, link}: MdxPage) => {
         <article>
             <p className={`text-right font-ming`}>
                 <Hyperlink
-                    href={'https://github.com/metashiyun/docs/blob/main/contents' + link + ".mdx"}>修改此页</Hyperlink>
+                    href={'https://github.com/metashiyun/docs/blob/main/content' + link + ".mdx"}>修改此页</Hyperlink>
             </p>
             <Title subtitle={subtitle}>{title}</Title>
             <MDXContents source={content}/>
